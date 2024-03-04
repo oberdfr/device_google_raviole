668c713be78f4a7363208b0c45e4d718f95388b2
Chirayu Desai <chirayudesai1@gmail.com>
2024-03-05 01:35:37 +0530

    proprietary-files: AP1A.240305.019.A1
    Change-Id: I519a099b4faefcb41da1c95a6ca704d9b1252944

--- raven/setup-makefiles-fix_build.sh
+++ raven/setup-makefiles-fix_build.sh
@@ -61,7 +61,7 @@ setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 write_headers
 
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
-write_makefiles "${MY_DIR}/proprietary-files-carriersettings.txt" true
+# write_makefiles "${MY_DIR}/proprietary-files-carriersettings.txt" true
 write_makefiles "${MY_DIR}/proprietary-files-vendor.txt" true
 
 # Remove defined PRODUCT_PACKAGES
